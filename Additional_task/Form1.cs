7361935f14e9ee0bb91f20ea64570b83f1d2984c
Anatolii_M <mapl175g@gmail.com>
2021-12-07 14:53:12 +0200

    Add project files.

---
+++ Additional_task/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Additional_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа byte от 0 до 255");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа sbyte от -128 до 127");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа short от -32 768 до 32767");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа ushort от 0 до 65535");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа int от -2147483648 до 2147483647");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа uint от 0 до 4294967295");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа long от -9223372036854775808 до 9223372036854775807");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Диапазон значений типа long от 0 до 18446744073709551615");
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// Используя Visual Studio, создайте проект по шаблону Console Application. 
+// Создать три строковые переменные и присвойте им значения: 
+// "\nмоя строка 1";
+// "\tмоя строка 2";
+// "\aмоя строка 3";
+// Выведите значение каждой переменной на экран.
+
+
+namespace AMelnyk_CSharpStarter_Lesson2_Machine_Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // На 18, 19 и 20 строках создаем переменные типа string
+            // с именами str1, str2, str3 и присваиваем им такст.
+            string str1 = "\n моя строка 1";
+            string str2 = "\t моя строка 1";
+            string str3 = "\a моя строка 1";
+
+            // На 23, 24 и и 25 стоках выводим значение переменных str1, str2, str3 на екран консоли.
+            Console.WriteLine("Эскейп последовательность - новая строка" + str1);
+            Console.WriteLine("Эскейп последовательность - табуляция" + str2);
+            Console.WriteLine("Эскейп последовательность - звуковой сигнал" + str3);
+
+            Console.ReadKey();
+        }
+    }
+}
